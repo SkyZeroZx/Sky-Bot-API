c8e8a1bdc9cac01d9414c77908ed0e726ef305c0
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2023-02-08 23:53:57 -0500

    (fix) : remove TODO

--- src/core/constants/mail.ts
+++ src/core/constants/mail.ts
@@ -1,12 +1,11 @@
-import { LOGO_APP } from "./notification";
+import { URL_WEB, LOGO_APP } from './notification';
 
- 
-// TODO : ADD LINK OF WEB FRONTEND FOR LOGIN
 export function MAIL_CREATE_USER(username: string, password: string) {
   return `<img src=${LOGO_APP}></img>
         <p>Estimado usuario se creado el nuevo usuario, en la app SkyKrono : ${username}
         \nSu contraseña es: <b> ${password}</b>
-        \nPara más detalle comunicarse con el area respectiva</p>`;
+        \nPara más detalle comunicarse con el area respectiva</p> 
+        <br />Puede logearse en el portal web : ${URL_WEB}`;
 }
 
 export function MAIL_RESET_USER(username: string, ticket: string) {
@@ -14,5 +13,6 @@ export function MAIL_RESET_USER(username: string, ticket: string) {
       <p>Estimado usuario se ha reseteado la contraseña de su usuario ${username}</p>
       <p>\nEl enlace para cambiar de contraseña es el siguiente: 
       <p><b> ${ticket} </b></p> 
-      \nPara más detalle comunicarse con el area respectiva</p>`;
+      \nPara más detalle comunicarse con el area respectiva</p>
+      <br />Puede logearse en el portal web : ${URL_WEB}`;
 }
